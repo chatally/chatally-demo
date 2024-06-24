1287ef9a92e229ddd401ea5b9f2a1c033e6fab48
Christian Fuss <christian@hackyourshack.org>
2024-06-24 14:29:51 +0000

    Updated the Github workflow
    - added script to restart server
    - using docker swarm mode with explicit building of docker image
    - added Traefik environment

---
+++ restart-server.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+# Use strict mode and setup defined execution context:
+set -euo pipefail
+IFS=$'\t\n'
+
+# Derive the site from the directory name, e.g. preview.dooiy.org
+caller_dir=${PWD}
+cd "$(dirname "$0")"
+site=${PWD##*/}
+stack="${site//./_}"
+
+echo "(Re-)starting stack ${stack} from ${PWD}..."
+
+# Stop if stack is running:
+if [[ -n $(docker stack ps "${stack}" 2>/dev/null) ]]; then
+  echo "⚠️ Stack '${stack}' is running, stopping it..." 1>&2
+  docker stack rm ${stack}
+  # The name is the name of the service
+  docker wait $(docker ps -q -f name=${stack}_app) 1>/dev/null 2>&1
+  echo -e "\b\b ✔️"
+fi
+
+# Setup environment variables from dotenv file
+echo -n "Setting up environment variables..."
+set -o allexport
+[[ -r "./.env" ]] && source ./.env
+set +o allexport
+echo -e "\b\b ✔️"
+
+echo "Building required docker image..."
+docker build --tag "chatally/demo-app" .
+echo -e "\b\b ✔️"
+
+## Run
+export SITE="${site}"
+export STACK="${stack}"
+echo "Starting your site..."
+docker stack deploy -c docker-compose.yml ${stack}
+echo -e "\b\b ✔️"
+
+# Provide a link to the site's entrypoints
+echo
+echo "☀️ Visit your new site (${stack}) at https://${site}"
